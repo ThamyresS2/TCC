64445bb9a89ba02ebf6e99674a76c30e89fa75b1
ThamyresS2 <thamyrestlms@gmail.com>
2026-08-17 10:07:04 -0400

    Tela de Incrição

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ addEventListener('DOMContentLoaded', function Login() {
                     </div>
                     <div id="buttons">
                         <button type="submit" onclick="PaginaParticipante()">Entrar</button>
+                        <button type="submit" id="inscrição" onclick="Inscrição()">Increva-se</button>
                         <button type="submit" id="area-servico" onclick="PaginaAreaServico()">Entrar Area Serviço</button>
                     </div>
                 </form>
@@ -23,6 +24,38 @@ addEventListener('DOMContentLoaded', function Login() {
         </body>
     </html>`;
 });
-function PaginaParticipante() {
-    window.location.href = "index.html";
+function PaginaAreaServico() {
+    const title = document.querySelector('title');
+    title.textContent = 'Área de Serviço';
+    const pagina = document.querySelector('body');
+    
+    pagina.innerHTML = `
+            <header>
+        <button id="addMonitores" onclick="AdicionarMonitores()">Adicionar Monitores</button>
+        <button id="addCordenador" onclick="AdicionarCoordenador()">Adicionar Coordenador</button>
+        <button id="addEvento" onclick="AdicionarEvento()">Adicionar Evento</button>
+        <button id="conta" onclick="MinhaConta()">Minha Conta</button>
+    </header>
+    `;
+}
+
+function Inscrição() {
+    const pagina = document.querySelector('body');
+    pagina.innerHTML = `
+        <div id="Incricao">
+        <img scr="logo.png">
+            <h1>Increva-se</h1>
+            <form action="">
+                <input type="text" id="nome" placeholder="Digite seu nome">
+                <input type="text" id="sobrenome" placeholder="Digite seu sobrenome">
+                <input type="text" id="idade" placeholder="Data de nascimento">
+                <input type="number" name="cpf" id="cpf" placeholder="CPF">
+                <input type="number" id="telefone" placeholder="Digite seu telefone">
+                <input type="email" name="email" id="email" placeholder="Digite seu email">
+                <input type="password" id="senha" placeholder="Digite sua senha">
+            </form>
+            <button id="btn" onclick="cadastrar()">Proximo</button>
+        </div>
+    `;
+
 }
